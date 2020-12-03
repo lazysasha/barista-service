abefb68c0ef7afc19bede9ed8669bc996481095e
Oleksandr (Sasha) Shynkariuk <oleksandr.shynkariuk@gmail.com>
2020-12-03 16:02:00 +0100

    solved 6th exercise - fault tolerance

--- src/main/java/nl/craftsmen/coffeehouse/PricesClient.java
+++ src/main/java/nl/craftsmen/coffeehouse/PricesClient.java
@@ -1,11 +1,15 @@
 package nl.craftsmen.coffeehouse;
 
 import nl.craftsmen.coffeehouse.models.Price;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import java.math.BigDecimal;
 
 /**
  * @author Oleksandr Shynkariuk sasha@routigo.com on 03/12/2020.
@@ -15,6 +19,15 @@ import javax.ws.rs.PathParam;
 public interface PricesClient {
 
     @GET
-    @Path("/prices/{productName}")
+    @Path("/prices/{productName}/v2")
+    @Retry(maxRetries = 2)
+    @Fallback(fallbackMethod = "fallback")
+    @CircuitBreaker
     Price get(@PathParam("productName") String productName);
+
+    default Price fallback(String productName) {
+        Price price = new Price();
+        price.price = BigDecimal.valueOf(42.0);
+        return price;
+    }
 }
